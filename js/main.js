1460771641287fe3fd60ed9c7cc4ae4645af7ae8
gabrielkheisa <gabrielkheisa@gmail.com>
2020-08-15 19:48:44 +0700

    tambah main.js buat judul dan footer universal

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var judul = "Gabriel Kheisa";
+var tahun = new Date().getFullYear()
+document.getElementById("judulweb").innerHTML = judul;
+document.getElementById("yearfooter").innerHTML = tahun + " " + judul;
